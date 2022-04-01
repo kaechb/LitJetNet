cd4627857c9b33ca28d17326fb670528fd1ce7ff
mova <kaechb@users.noreply.github.com>
2022-04-01 17:01:17 +0200

    iadded gan branch

--- lit_nf.py
+++ lit_nf.py
@@ -45,10 +45,6 @@ from collections import OrderedDict
 from ray import tune
 from helpers import *
 from plotting import *
-print("imports ok")
-plt.style.use(hep.style.ROOT)
-print(torch.cuda.is_available())
-
 
 
 class LitNF(pl.LightningModule):
@@ -58,19 +54,18 @@ class LitNF(pl.LightningModule):
                         in_features,
                         out_features,
                         hidden_features=self.config["network_nodes"],
-                        context_features=1 if self.config["conditional"] else None,
+                        context_features=1 if config["conditional"] else None,
                         num_blocks=self.config["network_layers"],
                         activation=FF.leaky_relu,
-                        dropout_probability=self.config["dropout"],
+                        dropout_probability=config["dropout"],
                         use_batch_norm=self.config["batchnorm"],
                         
                             )
     def __init__(self,config):
        
         super().__init__()
-         
+        self.n_dim=config["n_dim"]  
         self.config=config
-        self.n_dim=self.config["n_dim"] 
         self.batch_size=self.config["batch_size"]
         self.lr=self.config["lr"]
         self.losses=[]
@@ -81,7 +76,6 @@ class LitNF(pl.LightningModule):
         self.hparams.update(config)
         self.save_hyperparameters()
         self.flows = []
-        print(self.config["coupling_layers"])
         K=self.config["coupling_layers"]
         for i in range(K):
 #             mask=create_random_binary_mask(self.n_dim//3)
@@ -92,7 +86,7 @@ class LitNF(pl.LightningModule):
                     features=self.n_dim,
                     num_blocks=self.config["network_layers"], 
                     hidden_features=self.config["network_nodes"],
-                    context_features=1 if self.config["conditional"] else None,
+                    context_features=1 if config["conditional"] else None,
                     tails='linear',
                     tail_bound=3,
                     num_bins=self.config["bins"],
@@ -128,7 +122,7 @@ class LitNF(pl.LightningModule):
         self.q0 = nf.distributions.normal.StandardNormal([self.n_dim])
         self.flows=CompositeTransform(self.flows)
         # Construct flow model
-        self.flow = Flow(distribution=self.q0, transform=self.flows)
+        self.flow = base.Flow(distribution=self.q0, transform=self.flows)
     def load_datamodule(self,data_module):
         self.data_module=data_module
     def configure_optimizers(self):
@@ -144,34 +138,16 @@ class LitNF(pl.LightningModule):
             return({"optimizer": opt_flow,"lr_scheduler":lr_scheduler})
         else:
              return({"optimizer": opt_flow})
-#         if self.config["mse"]:
-#             opt_mse = torch.optim.Adam(self.parameters(), lr=lr)
-#             return opt_flow, opt_mse
-#         else:
-        
-    def get_metrics(self):
-        
-        # don't show the version number
-        items = super().get_metrics()
-        items.pop("v_num", None)
-        items.pop("loss", None)
-        return items
-#     def train_dataloader(self):        
-#         return DataLoader(self.data, batch_size=self.batch_size
-#                           ,shuffle=True,drop_last=True)
-           
-#     def training_epoch_end(self, training_step_outputs):
-#         print('training steps', training_step_outputs)
-        
-#         avg_loss = torch.mean(torch.stack([x["loss"]/self.n_dim for x in training_step_outputs]))
-# #         avg_logprob = torch.mean(torch.stack([x.logprob for x in training_step_outputs]))
-        
-#     def forward(self, x,c=None):
-#         # in lightning, forward defines the prediction/inference actions
-#         z,logprob = self.flow.transform_to_noise(x,c)
-#         return z,logprob
-    
-    
+    def build_discriminator(self):
+        net=[]
+        net.append(nn.Linear(self.n_dim,100))
+        for i in range(2):
+            net.append(nn.LeakyReLU())
+            net.append(nn.Linear(100,100))
+        net.append(nn.Linear(100,1))
+        net.append(nn.Sigmoid())
+        self.disc=nn.Sequential(*net)
+                
     def inverse(self,numsamples=1000,c=None):
         #Sample from gauss and transform from latent space to physical space
         if self.config["conditional"]:
@@ -179,16 +155,14 @@ class LitNF(pl.LightningModule):
         else:
             x = self.flow.sample(numsamples)
         return x
-    
-    
-    def training_step(self, batch, batch_idx,hyperopt=True):
-#         self.automatic_optimization=False
-        # access your optimizers with use_pl_optimizer=False. Default is True,
-#         if self.config["mse"]:
-#             opt_flow, opt_mse = self.optimizers(use_pl_optimizer=True)
-#         else:
-#             opt_flow = self.optimizers(use_pl_optimizer=True)
-
+    def adversarial_loss(self, y_hat, y):
+        return FF.binary_cross_entropy(y_hat, y)
+    def configure_optimizers(self):
+        lr = self.lr
+        opt_g = torch.optim.Adam(self.flow.parameters(), lr=lr)
+        opt_d = torch.optim.Adam(self.disc.parameters(), lr=lr)
+        return [opt_g, opt_d], []
+    def training_step(self, batch, batch_idx,optimizer_idx):
         if  self.config["conditional"]:
             x, c = batch[:,:self.n_dim],batch[:,self.n_dim].reshape(-1,1)
         
@@ -197,95 +171,78 @@ class LitNF(pl.LightningModule):
             c=None
         self.data_module.scaler.to(self.device)
         self.flow.to(self.device)
-        loss = -self.flow.log_prob(x,c).mean()/self.n_dim
-        self.logprobs.append(loss.detach().cpu().numpy())
-            
-        self.log("train_loss", loss, on_step=False, on_epoch=True, prog_bar=True, logger=True)  
-        if  self.global_step>self.config["n_mse"] and self.global_step<self.config["n_turnoff"]:# or  self.config["mse"] and self.current_epoch>self.config["n_mse"]:
         
-            gen=self.flow.sample(1,c.reshape(-1,1)).reshape(-1,self.n_dim).to(self.device)
+        # or  self.config["mse"] and self.current_epoch>self.config["n_mse"]:
+         
+        gen=self.flow.sample(1,c.reshape(-1,1)).reshape(-1,self.n_dim).to(self.device)
+        gen=self.data_module.scaler.inverse_transform(torch.hstack((gen[:,:self.n_dim]
+            .reshape(-1,self.n_dim),torch.ones(len(gen)).to(self.device).unsqueeze(1))))
+        m_g=mass(gen[:,:self.n_dim].to(self.device) ,
+                 self.config["canonical"]).to(self.device) 
+        true=self.data_module.scaler.inverse_transform(batch.to(self.device)).to(self.device)
+        m=true[:,self.n_dim]
+        if self.config["gan"]:
+            if optimizer_idx == 0:
+                g_loss = -self.flow.log_prob(x,c).mean()/self.n_dim
+                self.log("logprob", g_loss, on_step=False, on_epoch=True, prog_bar=True, logger=True) 
+                self.logprobs.append(g_loss.detach().cpu().numpy())
 
-            gen=self.data_module.scaler.inverse_transform(torch.hstack((gen[:,:self.n_dim]
-                .reshape(-1,self.n_dim),torch.ones(len(gen)).to(self.device).unsqueeze(1))))
-            m_g=mass(gen[:,:self.n_dim].to(self.device) ,
-                     self.config["canonical"]).to(self.device) 
-            true=self.data_module.scaler.inverse_transform(batch.to(self.device)).to(self.device)
-            m=true[:,self.n_dim]
-#             if self.global_step>1000:
-#                 _,b,_=plt.hist(m_g.detach().cpu().numpy(),alpha=0.3,bins=30)
-#                 plt.hist(m.detach().cpu().numpy(),alpha=0.3,bins=b)
-            
-#                 plt.savefig("test{}.png".format(self.global_step))
-#                 plt.close()
-#             if self.global_step>110:
-#                 raise
-            #loss=0 
-            mloss=self.config["lambda"]*FF.mse_loss(m_g.to(self.device).reshape(-1),m.to(self.device).reshape(-1))
-            if not hyperopt:
+               
+                mloss=self.config["lambda"]*FF.mse_loss(m_g.to(self.device).reshape(-1),m.to(self.device).reshape(-1))
                 self.log("mass_loss", mloss, on_step=False, on_epoch=True, prog_bar=True, logger=True)
-            else:
-                
-                self.log("mass_loss", mloss, on_step=False, on_epoch=True, prog_bar=False, logger=True)
-            loss+=mloss
-            self.mlosses.append(mloss.detach().cpu().numpy())
-            
-            
-#             loss=mloss
-#         return loss
+                if  self.global_step>self.config["n_mse"] and self.global_step<self.config["n_turnoff"]:
+                    g_loss+=mloss
+                # ground truth result (ie: all fake)
+                g_loss += self.adversarial_loss(self.disc(gen[:,:self.n_dim]), torch.ones(len(gen),1).to(self.device))
+                self.mlosses.append(mloss.detach().cpu().numpy())
+                self.losses.append(g_loss.detach().cpu().numpy())
+                self.log("loss", g_loss, on_step=False, on_epoch=True, prog_bar=True, logger=True)
+                return OrderedDict({"loss":g_loss})
+            # train discriminator
+            if optimizer_idx == 1:
+                # Measure discriminator's ability to classify real from generated samples
 
-        self.losses.append(loss.detach().cpu().numpy())
-        # if  self.global_step>self.config["n_mse"] and self.global_step<self.config["n_turnoff"]:
-        #     tune.report(step=self.global_step,mloss=self.mlosses[-1],logprob=self.logprobs[-1],loss=self.losses[-1])    
-        # else:
-        #     tune.report(step=self.global_step,mloss=0,logprob=self.logprobs[-1],loss=self.losses[-1])    
-        output = OrderedDict({
-#             'loss': loss.detach(),
-            "loss": loss,
-#             "mloss": mloss.detach(),
-#             'progress_bar': tqdm_dict,
-#             'log': tqdm_dict
-        })
-        return output
-    def validation_step(self, batch, batch_idx,hyperopt=True):
-        self.data_module.scaler.to("cpu")
-        
+                # how well can it label as real?
+                valid = torch.ones(len(batch),1).to(self.device)
+                real_loss = self.adversarial_loss(self.disc(batch[:,:self.n_dim]), valid)
+
+                # how well can it label as fake?
+                fake = torch.zeros(len(gen),1).to(self.device)
+                fake_loss = self.adversarial_loss(self.disc(gen[:,:self.n_dim].detach()), fake)
+
+                # discriminator loss is the average of these
+                d_loss = (real_loss + fake_loss) / 2
+                self.log("d_loss", d_loss, on_step=False, on_epoch=True, prog_bar=True, logger=True)
+                return OrderedDict({"loss":d_loss})
+      
+#         output = OrderedDict({
+#           "loss": loss,
+#         })
+#         return output
+    def validation_step(self, batch, batch_idx):
+        self.data_module.scaler.to("cpu")        
         with torch.no_grad():
             if self.config["conditional"]:
                 gen=self.flow.to("cpu").sample(1,batch[:,self.n_dim].reshape(-1,1).to("cpu")).to("cpu")
                 gen=self.data_module.scaler.inverse_transform(torch.hstack((gen[:,:self.n_dim].cpu().detach()
                     .reshape(-1,self.n_dim),torch.ones(len(gen)).unsqueeze(1))))
-                
             else:
                 gen=self.flow.sample(len(batch)).to("cpu")
                 gen=self.data_module.scaler.inverse_transform(torch.hstack((gen[:,:self.n_dim]
                     .cpu().detach().reshape(-1,self.n_dim),torch.ones(len(gen)).unsqueeze(1))))
         true=self.data_module.scaler.inverse_transform(batch.cpu())
-        
         m_t=true[:,self.n_dim].to(self.device)
         m_gen=mass(gen[:,:self.n_dim],self.config["canonical"])
-#         gen_corr=torch.clone(gen[:,:self.n_dim]).reshape(-1,self.n_dim//3,3)
-#         gen_corr[gen_corr[:,:,2]<0]=0
-#         cov,mmd=cov_mmd(gen_corr,true[:,:self.n_dim].reshape(-1,self.n_dim//3,3),use_tqdm=False)
-#         self.log("cov",cov, prog_bar=True, logger=True)
-#         self.log("mmd",mmd, prog_bar=True, logger=True)
-        gen[:,self.n_dim]=m_gen
         self.metrics["w1p"].append(w1p(gen[:,:self.n_dim].reshape(-1,self.n_dim//3,3).numpy(),true[:,:self.n_dim].reshape(-1,self.n_dim//3,3)))
         self.metrics["w1m"].append(w1m(gen[:,:self.n_dim].reshape(-1,self.n_dim//3,3).numpy(),true[:,:self.n_dim].reshape(-1,self.n_dim//3,3)))
         self.metrics["w1efp"].append(w1efp(gen[:,:self.n_dim].reshape(-1,self.n_dim//3,3).numpy(),true[:,:self.n_dim].reshape(-1,self.n_dim//3,3)))
-        
-        if not hyperopt:
-            self.log("val_w1m",self.metrics["w1m"][-1][0],on_step=False, on_epoch=True, prog_bar=True, logger=True)
-        else:
-            tune.report(w1m=self.metrics["w1m"][-1][0],w1p=self.metrics["w1p"][-1][0],w1efp=self.metrics["w1efp"][-1][0],logprob=self.logprobs[-1] if len(self.logprobs)>0 else None
-            ,mloss=self.mlosses[-1] if len(self.mlosses)>0 else None) 
-            self.log("val_w1m",self.metrics["w1m"][-1][0],on_step=False, on_epoch=True, prog_bar=False, logger=True)
-        self.plot=plotting(model=self,gen=gen,true=true,config=self.config,step=self.global_step,logger=self.logger.experiment)
+        self.log("val_w1m",self.metrics["w1m"][-1][0],on_step=False, on_epoch=True, prog_bar=True, logger=True)
+        self.plot=plotting(model=self,gen=gen,true=true,config=config,step=self.global_step,logger=self.logger.experiment)
         try:
             self.plot.plot_mass(save=True,quantile=True)
-            # self.plot.plot_marginals(save=True)
+#             self.plot.plot_marginals(save=True)
             self.plot.plot_2d(save=True)
             self.plot.losses(save=True)
-
         except Exception as e:
             traceback.print_exc()
         self.flow.to("cuda")
