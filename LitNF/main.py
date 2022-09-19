045ec4c5ab3fe8756a3b2775cb0059892e1c84f9
kaechb <benno.kaech@hotmail.com>
2022-09-19 08:44:28 +0200

    some cleaning done

--- LitNF/main.py
+++ LitNF/main.py
@@ -17,11 +17,12 @@ from helpers import *
 from jetnet_dataloader import JetNetDataloader
 from lit_nf import TransGan
 from plotting import plotting
+import yaml
 
 # from comet_ml import Experiment
 
 
-def train(config, hyperopt=False, load_ckpt=None, i=0, root=None):
+def train(config,  load_ckpt=None, i=0, root=None):
     # This function is a wrapper for the hyperparameter optimization module called ray
     # Its parameters hyperopt and load_ckpt are there for convenience
     # Config is the only relevant parameter as it sets the trainings hyperparameters
@@ -29,7 +30,7 @@ def train(config, hyperopt=False, load_ckpt=None, i=0, root=None):
     data_module = JetNetDataloader(config)  # this loads the data
     data_module.setup("training")
     model = TransGan(
-        config, hyperopt, data_module.num_batches
+        config,  data_module.num_batches
     )  # the sets up the model,  config are hparams we want to optimize
     model.data_module = data_module
     # Callbacks to use during the training, we  checkpoint our models
@@ -79,52 +80,7 @@ def train(config, hyperopt=False, load_ckpt=None, i=0, root=None):
 
 if __name__ == "__main__":
 
-    hyperopt = False  # This sets to run a hyperparameter optimization with ray or just running the training once
-
-    # config = {
-    #     "autoreg": False,
-    #     "context_features": 0,
-    #     "network_layers": 3,  # sets amount hidden layers in transformation networks -scannable
-    #     "network_layers_nf": 2,  # sets amount hidden layers in transformation networks -scannable
-    #     "network_nodes_nf": 256,  # amount nodes in hidden layers in transformation networks -scannable
-    #     "batch_size": 2000,  # sets batch size -scannable #best one 4000
-    #     "coupling_layers": 15,  # amount of invertible transformations to use -scannable
-    #     "lr": 0.001,  # sets learning rate -scannable
-    #     "batchnorm": False,  # use batchnorm or not -scannable
-    #     "bins": 5,  # amount of bins to use in rational quadratic splines -scannable
-    #     "tail_bound": 6,  # splines:max value that is transformed, over this value theree is id  -scannable
-    #     "limit": 150000,  # how many data points to use, test_set is 10% of this -scannable in a sense use 10 k for faster training
-    #     "n_dim": 3,  # how many dimensions to use or equivalently /3 gives the amount of particles to use NEVER EVER CHANGE THIS
-    #     "dropout": 0.2,  # use droput proportion, for 0 there is no dropout -scannable
-    #     "canonical": False,  # transform data coordinates to px,py,pz -scannable
-    #     "max_steps": 100000,  # how many steps to use at max - lower for quicker training
-    #     "lambda": 100,  # balance between massloss and nll -scannable
-    #     "name": "Transflow_best",  # name for logging folder
-    #     "disc": False,  # whether to train gan style discriminator that decides whether point is simulated or generated-semi-scannable
-    #     "variable": 1,  # use variable amount of particles otherwise only use 30, options are true or False
-    #     "parton": "t",  # choose the dataset you want to train options: t for top,q for quark,g for gluon
-    #     "wgan": False,
-    #     "corr": True,
-    #     "num_layers": 5,
-    #     "freq": 10,
-    #     "n_part": 30,
-    #     "fc": False,
-    #     "hidden": 80,
-    #     "heads": 3,
-    #     "l_dim": 63,
-    #     "lr_g": 1e-4,
-    #     "lr_d": 1e-4,
-    #     "lr_nf": 0.000722,
-    #     "sched": "cosine2",
-    #     "opt": "SGD",
-    #     "lambda": 1,
-    #     "max_epochs": 1600,
-    #     "mass": True,
-    #     "no_hidden": True,
-    #     "clf": True,
-    #     "val_check": 50,
-    #     "frac_pretrain": 80,
-    # }
+ 
     cols = [
         "name",
         "parton",
@@ -143,101 +99,18 @@ if __name__ == "__main__":
         "num_layers",
         "val_check",
     ]
-    config = {
-        "autoreg": False,
-        "context_features": 0,
-        "network_layers": 3,
-        "network_layers_nf": 2,
-        "network_nodes_nf": 256,
-        "batch_size": 1024,
-        "coupling_layers": 15,
-        "lr": 0.001,
-        "batchnorm": False,
-        "bins": 5,
-        "tail_bound": 6,
-        "limit": 200000,
-        "n_dim": 3,
-        "dropout": 0.2,
-        "canonical": False,
-        "max_steps": 100000,
-        "lambda": 100,
-        "name": "Transflow_best",
-        "disc": False,
-        "variable": 1,
-        "parton": "t",
-        "wgan": True,
-        "corr": True,
-        "num_layers": 4,
-        "freq": 6,
-        "n_part": 30,
-        "fc": False,
-        "hidden": 500,
-        "heads": 6,
-        "l_dim": 25,
-        "lr_g": 0.0004327405312571664,
-        "lr_d": 0.0004327405312571664,
-        "lr_nf": 0.000722,
-        "sched": "cosine",
-        "opt": "Adam",
-        "max_epochs": 1600,
-        "mass": True,
-        "no_hidden": False,
-        "clf": True,
-        "val_check": 50,
-        "frac_pretrain": 80,
-        "seed": 69,
-        "quantile": False,
-    }  #'seed': 744,sched:"None","wgan":False,"freq":8,"sched":None,"heads":4
-    config={'autoreg': False, 'context_features': 0, 'network_layers': 3, 'network_layers_nf': 2, 'network_nodes_nf': 256, 'batch_size': 1024, 'coupling_layers': 15, 'lr': 0.001, 'batchnorm': False, 'bins': 5, 'tail_bound': 6, 'limit': 150000, 'n_dim': 3, 'dropout': 0.2, 'canonical': False, 'max_steps': 100000, 'lambda': 1, 'name': 'Transflow_best', 'disc': False, 'variable': 1, 'parton': 't', 'wgan': False, 'corr': True, 'num_layers': 4, 'freq': 6, 'n_part': 30, 'fc': False, 'hidden': 500, 'heads': 4, 'l_dim': 25, 'lr_g': 0.0004327405312571664, 'lr_d': 0.0004327405312571664, 'lr_nf': 0.000722, 'sched': "cosine2", 'opt': 'RMSprop', 'max_epochs': 3200, 'mass': True, 'no_hidden': False, 'clf': True, 'val_check': 50, 'frac_pretrain': 80,"seed":69,"quantile": False, }#'seed':
-    config["l_dim"] = config["l_dim"] * config["heads"]
-
-    print(config["name"])
-
+    best_hparam="/beegfs/desy/user/kaechben/Transflow_best/lightning_logs/version_72/hparams.yaml"
+    with open(best_hparam, 'r') as stream:
+        config=yaml.load(stream,Loader=yaml.Loader)
+        print(config)
+        config=config["config"]
     if len(sys.argv) > 2:
         root = "/beegfs/desy/user/"+ os.environ["USER"]+"/"+config["name"]+"/"+config["parton"]+"_" +"run"+sys.argv[1]+"_"+str(sys.argv[2])
     else:
         root = "/beegfs/desy/user/" + os.environ["USER"] + "/"+ config["name"]
-    if not hyperopt:
-        hyperopt = True
+
         for col in cols:
             print('"' + col + '":' + str(config[col]))
 
-        train(config, hyperopt=hyperopt, root=root)
-    else:
-        # if not os.path.isfile("/beegfs/desy/user/{}/ray_results/{}/summary.csv".format(os.environ["USER"],config["parton"])):
-        #     pd.DataFrame().to_csv("/beegfs/desy/user/{}/ray_results/{}/summary.csv".format(os.environ["USER"],config["parton"]))
-        num_samples = 1  # how many hparam settings to sample with ray
-        resources = {"cpu": 10, "gpu": 0.5}
-        # This sets the logging in ray
-        # reporter = CLIReporter(max_progress_rows=40, max_report_frequency=300, sort_by_metric=True,
-        #                        metric="logprob", parameter_columns=["network_nodes", "network_layers", "coupling_layers", "lr"])
-        for i in range(num_samples):
-
-            temproot = root
-
-            config["sched"] = np.random.choice(["cosine", "cosine2", None])
-            #config["opt"] = np.random.choice(["Adam", "RMSprop"])
-            #config["mass"] = np.random.choice([True, False])
-            config["quantile"] = np.random.choice([True, False])
-            config["no_hidden"] = np.random.choice([True, False,"more"])
-            config["clf"] = np.random.choice([True, False])
-            # config["batch_size"] = 2 ** np.random.randint(8, 12)
-            config["freq"] = np.random.randint(5, 10)
-            config["seed"] = int(np.random.randint(1, 1000))
-            
-            # config["lr_g"] = stats.loguniform.rvs(0.00001, 0.001, size=1)[0]
-            
-            
-            config["heads"] = np.random.randint(3, 6)
-            config["l_dim"] = config["heads"] * 25
-            # config["hidden"] = 100 * np.random.randint(2, 7)
-            config["num_layers"] = np.random.randint(3, 6)
-
-            for col in cols:
-                print('"' + col + '":' + str(config[col]))
-
-            try:
-                train(config, hyperopt=hyperopt, i=i, root=temproot)
-            except:
-                print("error")
-                traceback.print_exc()
+        train(config, root=root)
+   
