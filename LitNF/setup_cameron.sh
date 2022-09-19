75efe271425ab430ee5c737829fc05c3045061d8
kaechb <benno.kaech@hotmail.com>
2022-09-19 08:57:07 +0200

    some cleaning done

--- LitNF/setup_cameron.sh
+++
@@ -1,5 +0,0 @@
-"module load anaconda-python/3.8 maxwell gcc/9.3
-. conda-init
-alias mine='salloc --partition=cms-desy --time=24:00:00 --nodes=1 --constraint="P100"'
-alias renewal='klist -s ||kinit'" >> .bashrc
-conda env create -f ray_lightning.yml
